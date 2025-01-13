387bf6d7f74a0daf6a7bb0b569c0c69b8efb98b7
John Puddephatt <john@jdp.org.uk>
2025-01-13 20:53:22 +0000

    spacing

--- resources/views/blocks/newsletter.blade.php
+++ resources/views/blocks/newsletter.blade.php
@@ -2,15 +2,15 @@
     style="{{ $block->inlineStyle }}">
 
     <div
-      class="{{ $block->block->align == 'full' ? '' : 'rounded-3xl my-16 2xl:my-24' }} relative flex flex-row items-center justify-between gap-6 bg-beige p-8 xl:p-16">
+      class="{{ $block->block->align == 'full' ? '' : 'rounded-3xl my-16 2xl:my-24' }} relative flex flex-row justify-between gap-6 bg-beige p-8 xl:p-16">
       <div
         class="{{ $block->block->align == 'full' ? 'max-w-xl text-xl' : 'max-w-lg' }} flex flex-col justify-around py-8">
         <div>
           <h1 class="type-xl mb-4">{{ $heading }}</h1>
           <div>{!! $content !!}</div>
         </div>
-        <form id="mc-embedded-subscribe-form" class="validate mt-auto pt-4" action="{{ $mailchimp_form_url }}"
-          method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
+        <form id="mc-embedded-subscribe-form" class="validate pt-4" action="{{ $mailchimp_form_url }}" method="post"
+          name="mc-embedded-subscribe-form" novalidate="" target="_blank">
           <div style="position: absolute; left: -5000px;" aria-hidden="true"><input tabindex="-1"
               name="b_27860ab2b8185d0189fb1222c_b2ae2b005f" type="text" value=""></div>
 
