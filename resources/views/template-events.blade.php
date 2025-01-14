c62daf70eb402c33b6a6d5cfbc3f6585712f03cc
John Puddephatt <john@jdp.org.uk>
2025-01-14 10:27:38 +0000

    tidy events filter

--- resources/views/template-events.blade.php
+++ resources/views/template-events.blade.php
@@ -12,16 +12,17 @@
   {!! $content !!}
 
   <div id="events" class="container mb-24 space-y-8 lg:max-w-5xl">
-    <p>{{ $event_count }} events @ {{ $per_page }} per page = {{ ceil($event_count / $per_page) }} pages</p>
+    {{-- <p>{{ $event_count }} events @ {{ $per_page }} per page = {{ ceil($event_count / $per_page) }} pages</p> --}}
 
-    <div>
-      <x-button :class="!isset($_GET['date']) || $_GET['date'] == 'all' ? 'bg-blue-light' : null" :url="setParam('date', 'all')" label="All dates" />
-      <x-button :class="isset($_GET['date']) && $_GET['date'] == 'this_week' ? 'bg-blue-light' : null" :url="setParam('date', 'this_week')" label="This week" />
-      <x-button :class="isset($_GET['date']) && $_GET['date'] == 'next_week' ? 'bg-blue-light' : null" :url="setParam('date', 'next_week')" label="Next week" />
+    <div class="flex justify-between">
+      <div>
+        <x-button :class="!isset($_GET['date']) || $_GET['date'] == 'all' ? 'bg-blue-light' : null" :url="setParam('date', 'all')" label="All dates" />
+        <x-button :class="isset($_GET['date']) && $_GET['date'] == 'this_week' ? 'bg-blue-light' : null" :url="setParam('date', 'this_week')" label="This week" />
+        <x-button :class="isset($_GET['date']) && $_GET['date'] == 'next_week' ? 'bg-blue-light' : null" :url="setParam('date', 'next_week')" label="Next week" />
+      </div>
+
+      <x-button :class="isset($_GET['category']) && $_GET['category'] == 'all' ? 'bg-blue-light' : null" :url="setParam('category', 'all')" label="All categories" />
     </div>
-
-    <x-button :class="isset($_GET['category']) && $_GET['category'] == 'something' ? 'bg-blue-light' : null" :url="setParam('category', 'all')" label="A category" />
-
     @if (!$events)
       <x-alert type="warning" class="lg:max-w-5xl">
         {!! __('Sorry, no events were found.', 'sage') !!}
