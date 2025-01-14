38b6d16a8242d5e7e1ed134a261519fac2c27593
John Puddephatt <john@jdp.org.uk>
2025-01-14 12:28:41 +0000

    toc

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -3,7 +3,7 @@
 
     @if (has_post_thumbnail($post->ID))
       <div class="relative -mb-32 ml-auto w-1/2 max-w-4xl">
-        <svg class="absolute right-2/3 top-[20%] h-auto w-[60%]" xmlns="http://www.w3.org/2000/svg" width="107.4"
+        <svg class="absolute right-2/3 top-[15%] h-auto w-[60%]" xmlns="http://www.w3.org/2000/svg" width="107.4"
           height="107.43" viewBox="0 0 107.4 107.43">
           <path fill="#ebdbd1"
             d="M13.46 90.73c6.53 8.66 15.94 16 26.77 16.65a40.15 40.15 0 0 0 13.89-2.05c28-8.44 55.62-32.63 53.12-63.8-1.38-17.27-18.78-30.24-33.86-36.62-13-5.5-28-6.57-41.38-2.16C13.35 8.88 3.11 22.18.6 40.89a68.51 68.51 0 0 0 12.86 49.84Z"
