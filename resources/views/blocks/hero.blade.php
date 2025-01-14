b20c7694cfa09609bf3b4ce7508609ca9f12549b
John Puddephatt <john@jdp.org.uk>
2025-01-14 11:12:09 +0000

    containerise

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -3,7 +3,7 @@
   style="{{ $block->inlineStyle }}">
 
   <div class="{{ $block->block->align == 'full' ? 'container gap-6' : null }} relative grid grid-cols-2 items-center">
-    <div class="{{ $block->block->align == 'full' ? 'py-16' : ' p-8 lg:p-16' }} relative max-w-xl">
+    <div class="{{ $block->block->align == 'full' ? 'py-16 max-w-xl' : ' p-8 lg:p-16' }} relative">
       <h1 class="type-xl mb-6 text-blue-dark">{{ $heading }}</h1>
       <div class="text-xl">{!! $content !!}</div>
       @if ($buttons)
