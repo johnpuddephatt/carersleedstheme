314865347bad77d4a68d5df12cade5c8769257f7
John Puddephatt <john@jdp.org.uk>
2025-01-14 12:28:01 +0000

    toc

--- resources/views/blocks/opportunities-list.blade.php
+++ resources/views/blocks/opportunities-list.blade.php
@@ -1,6 +1,6 @@
-<div class="{{ $block->classes }} not-prose my-16 2xl:my-24" style="{{ $block->inlineStyle }}">
+<div class="{{ $block->classes }} my-16 2xl:my-24" style="{{ $block->inlineStyle }}">
   <h2 class="type-lg mb-12 text-blue-dark">{{ $title }}</h2>
-  <div class="flex flex-col gap-4">
+  <div class="not-prose flex flex-col gap-4">
     @forelse ($opportunities as $opportunity)
       <x-opportunity-card :opportunity="$opportunity" />
     @empty
