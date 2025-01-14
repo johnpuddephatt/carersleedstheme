7c2c2cbedfbe24f79e2449968328ca3342aef838
John Puddephatt <john@jdp.org.uk>
2025-01-14 12:22:53 +0000

    border on toc

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,7 +1,8 @@
 <article @php(post_class())>
   <div class="container mx-auto min-h-screen xl:max-w-5xl 2xl:max-w-6xl">
     <div class="flex flex-col gap-12 pb-32 lg:flex-row-reverse">
-      <div class="max-w-screen-sm max-lg:bg-pink max-lg:p-4 max-sm:-mx-4 lg:w-48 xl:pt-16">
+      <div
+        class="max-w-screen-sm border-t border-blue-bright max-lg:rounded-3xl max-lg:bg-blue-light max-lg:p-4 max-sm:-mx-4 lg:w-48 xl:pt-16">
         <div class="top-16 mx-auto lg:sticky">
           {!! $toc !!}
         </div>
