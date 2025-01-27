727dd97b6bd1f56b4b7ba9269d459b54d2134e4d
John Puddephatt <john@jdp.org.uk>
2025-01-27 13:51:52 +0000

    debug socials

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -9,11 +9,10 @@
         <div class="flex flex-row items-start gap-2 md:mt-2 lg:justify-end">
           @if (get_field('social_media', 'option'))
             @foreach (get_field('social_media', 'option') as $account)
-              {{ $account['Type'] }}•••{{ $account['link'] }}••••••
-              {{-- <a rel="noopener" class="inline-block rounded-full bg-white p-2 text-blue"
+              <a rel="noopener" class="inline-block rounded-full bg-white p-2 text-blue"
                 aria-label="{{ $account['Type'] }} link" href="{{ $account['link'] }}" target="_blank">
-                <x-dynamic-component :component="'icon.' . $account['Type']" class="mt-4" />
-              </a> --}}
+                <x-dynamic-component :component="'icon.' . strtolower($account['Type'])" class="mt-4" />
+              </a>
             @endforeach
           @endif
         </div>
