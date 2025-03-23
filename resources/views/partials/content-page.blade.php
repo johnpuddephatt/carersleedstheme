8509ead3c187e22e09934afa7c2a2b9c1b6d95e3
John Puddephatt <john@jdp.org.uk>
2025-03-23 19:43:06 +0000

    fix gap when no toc

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,9 +1,9 @@
 <article @php(post_class())>
   <div class="container mx-auto xl:max-w-5xl 2xl:max-w-6xl">
-    <div class="{{ $toc ? 'gap-12' : null }} flex flex-col pb-12 lg:flex-row-reverse lg:justify-end lg:pb-32">
+    <div class="flex flex-col gap-12 pb-12 lg:flex-row-reverse lg:justify-end lg:pb-32">
 
       <div
-        class="{{ $toc ? 'max-lg:bg-blue-light xl:pt-16 max-lg:p-4 ' : null }} max-lg:rounded-medium max-w-screen-sm lg:w-48">
+        class="{{ $toc ? 'max-lg:bg-blue-light xl:pt-16 max-lg:p-4 ' : null }} max-w-screen-sm max-lg:rounded-medium lg:w-48">
         @if ($toc)
           <div class="mx-auto pt-4 lg:sticky lg:top-12 lg:border-t lg:border-blue-bright">
             {!! $toc !!}
