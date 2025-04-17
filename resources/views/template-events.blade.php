ef5ec6de3f9a8751c39eca4ebf349039ae85ec24
John Puddephatt <john@jdp.org.uk>
2025-04-17 12:09:30 +0100

    fix location filters

--- resources/views/template-events.blade.php
+++ resources/views/template-events.blade.php
@@ -36,9 +36,8 @@
 
       <h2 class="type-lg !mt-12 text-center text-blue-dark">Find an event near you</h2>
       <div class="grid grid-cols-2 gap-4 md:grid-cols-3">
-        <x-button-pill :class="!isset($_GET['location']) || $_GET['location'] == 'all' ? 'bg-blue-light' : null" :url="get_permalink(get_option('page_for_events'))" label="All" />
-        <x-button-pill :class="isset($_GET['location']) && $_GET['location'] == 'south' ? 'bg-blue-light' : null" :url="setParam('location', 'south')" label="South Leeds" />
         <x-button-pill :class="isset($_GET['location']) && $_GET['location'] == 'north' ? 'bg-blue-light' : null" :url="setParam('location', 'north')" label="North Leeds" />
+        <x-button-pill :class="isset($_GET['location']) && $_GET['location'] == 'south' ? 'bg-blue-light' : null" :url="setParam('location', 'south')" label="South Leeds" />
         <x-button-pill :class="isset($_GET['location']) && $_GET['location'] == 'east' ? 'bg-blue-light' : null" :url="setParam('location', 'east')" label="East Leeds" />
         <x-button-pill :class="isset($_GET['location']) && $_GET['location'] == 'west' ? 'bg-blue-light' : null" :url="setParam('location', 'west')" label="West Leeds" />
         <x-button-pill :class="isset($_GET['location']) && $_GET['location'] == 'leeds-city-centre' ? 'bg-blue-light' : null" :url="setParam('location', 'leeds-city-centre')" label="Leeds City Centre" />
@@ -60,8 +59,12 @@
     <div class="container my-16 space-y-4 md:mb-24 md:space-y-8 lg:max-w-5xl">
 
       <a href="{{ get_permalink(get_option('page_for_events')) }}">&larr; Back to all events</a>
-      <h1 class="type-xl">{{ ucfirst($_GET['type'] ?? 'All') }} events in {{ ucfirst($_GET['location'] ?? '') }} Leeds
+      <h1 class="type-xl">{{ ucfirst($_GET['type'] ?? 'All') }} events in
+        {{ ucwords(str_replace('-', ' ', $_GET['location'] ?? '')) }}
+        {{ strpos($_GET['location'] ?? '', 'leeds') !== 0 ? 'Leeds' : '' }}</h1>
+
       </h1>
+      {{ strpos($_GET['location'] ?? '', 'leeds') }}
 
     </div>
   @endif
