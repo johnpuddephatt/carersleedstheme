76ca566f8a06bc94dbcce923413f980ef79f4952
John Puddephatt <john@jdp.org.uk>
2025-04-24 22:40:51 +0100

    search card

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,15 +1,18 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
+<a href="{{ get_permalink() }}"
+  class="not-prose group relative flex min-h-32 flex-row items-center overflow-hidden rounded-medium bg-green-light font-normal after:absolute after:right-1 after:top-2 after:block after:size-6 after:rounded-full after:bg-green md:min-h-48 md:after:size-8">
 
-    @includeWhen(get_post_type() === 'post', 'partials.entry-meta')
-  </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
+  <div class="flex-1 px-8 py-2 md:pl-4 lg:pl-8 lg:pr-12">
+    <div class="mb-1 leading-snug text-blue">
+      @includeWhen(get_post_type() === 'post', 'partials.entry-meta')
+    </div>
   </div>
-</article>
+
+  <h3 class="type-sm md:type-md text-balance">{!! $title !!}
+  </h3>
+
+  <div class="mt-3 hidden max-w-3xl md:block">
+    @php(the_excerpt())
+
+  </div>
+
+</a>
