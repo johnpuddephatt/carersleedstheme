dec5fafcb7e79f2ea1a78bef21991c31d05e947a
John Puddephatt <john@jdp.org.uk>
2025-04-24 22:49:47 +0100

    search form

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,14 +7,14 @@
     <x-alert type="warning">
       {!! __('Sorry, no results were found.', 'sage') !!}
     </x-alert>
-
-    {!! get_search_form(false) !!}
   @else
     <div class="container mx-auto space-y-8 lg:max-w-5xl">
       <h1 class="type-xl my-16 text-blue-dark">
         {!! __('Search results for', 'sage') !!} <strong>{!! get_search_query() !!}</strong></h1>
     </div>
   @endif
+
+  {!! get_search_form(false) !!}
 
   <div class="container mx-auto space-y-8 lg:max-w-5xl">
     @while (have_posts())
