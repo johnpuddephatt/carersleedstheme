6f4c7d10cf747d27c6446aa66d8635ac0517959d
John Puddephatt <john@jdp.org.uk>
2025-04-24 22:46:53 +0100

    padding

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,8 +1,8 @@
 <a href="{{ get_permalink() }}"
   class="not-prose {{ get_post_type() === 'post' ? 'bg-green-light after:rounded-full after:bg-green md:min-h-48 md:after:size-8' : 'bg-beige' }} group relative flex min-h-32 flex-row items-center overflow-hidden rounded-medium font-normal after:absolute after:right-1 after:top-2 after:block after:size-6">
 
-  <div>
-    <div class="flex-1 px-8 py-2 md:pl-4 lg:pl-8 lg:pr-12">
+  <div class="flex-1 px-8 py-2 md:pl-4 lg:pl-8 lg:pr-12">
+    <div class="">
       <div class="mb-1 leading-snug text-blue">
         @includeWhen(get_post_type() === 'post', 'partials.entry-meta')
       </div>
