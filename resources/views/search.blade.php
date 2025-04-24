488d1ae1c45281abcb9d9d3873011736b948a95d
John Puddephatt <john@jdp.org.uk>
2025-04-24 22:48:35 +0100

    search heading

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,7 +11,7 @@
     {!! get_search_form(false) !!}
   @else
     <h1 class="type-xl my-16 text-blue-dark">
-      {!! __('Search results for', 'sage') !!} <strong>{!! get_search_query() !!}</strong>
+      {!! __('Search results for', 'sage') !!} <strong>{!! get_search_query() !!}</strong></h1>
   @endif
 
   <div class="container mx-auto space-y-8 lg:max-w-5xl">
