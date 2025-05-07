95598cb2689f8c9903d7a2696ca123353ccf02b4
John Puddephatt <john@jdp.org.uk>
2025-05-07 15:17:44 +0100

    filter past events

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -61,7 +61,7 @@
                 x-text="totalHits == 1 ? null : 's'"></span>.</p>
 
             <template x-for="result in results"><a
-                x-show="result.post_type !== 'tribe_events' || new Date(result._EventStartDate) > new Date()"
+                x-show="(result.post_type !== 'tribe_events') || new Date(result._EventStartDate) > new Date()"
                 :href="result.permalink"
                 class="flex items-center gap-4 border-b border-blue-light px-8 py-4 transition hover:bg-blue-light hover:bg-opacity-20">
                 <img x-show="result.images.thumbnail"
