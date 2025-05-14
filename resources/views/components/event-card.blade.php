d7bb7e7484567010346dae3c1ac2fbeaa5fa8cb4
John Puddephatt <john@jdp.org.uk>
2025-05-14 16:39:10 +0100

    event card

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -7,7 +7,7 @@
           match ($variant) {
               'outlined' => 'border-2 border-gold',
               default
-                  => ' bg-gold-light after:absolute after:right-1 after:top-2 after:block after:size-6 md:after:size-8 after:rounded-full after:bg-gold',
+                  => ' bg-gold-light after:absolute after:right-0 after:top-1 md:after:right-1 md:after:top-2 after:block after:size-6 md:after:size-8 after:rounded-full after:bg-gold',
           },
   ]) }}>
 
@@ -20,7 +20,7 @@
       ]) !!}
     </div>
   @endif
-  <div class="py-2 pl-2 pr-4 md:py-4 md:pl-4 md:pr-12 lg:pl-8">
+  <div class="py-2 pl-2 pr-2 md:py-4 md:pl-4 md:pr-12 lg:pl-8">
 
     <div class="leading-snug">
       {!! tribe_get_start_date($event->ID, false, get_option('date_format')) !!}
@@ -41,7 +41,7 @@
 
       <p class="flex items-start leading-tight">
         <span class="-mt-0.5 mr-1 inline-block rounded-full bg-gold p-0.5 md:mr-2">
-          <x-icon.marker class="h-6 w-6 text-white" />
+          <x-icon.marker class="h-4 w-4 text-white md:h-6 md:w-6" />
         </span>
         {!! tribe_get_venue($event->ID) !!}
         @if (tribe_get_address($event->ID))
