a6cbc3273d0cb75a79423d7a92f2e41ff51c32e8
John Puddephatt <john@jdp.org.uk>
2025-05-22 11:04:16 +0100

    pagin

--- resources/views/events/summary.blade.php
+++ resources/views/events/summary.blade.php
@@ -15,13 +15,12 @@
 
 <div class="mt-12 text-right text-xl">
   {{-- 'base' => get_permalink(get_queried_object_id()), --}}
-
+  {{ str_replace(9999, '%#%', esc_url(get_pagenum_link(9999))) }}
   {!! paginate_links([
       'base' => str_replace(9999, '%#%', esc_url(get_pagenum_link(9999))),
   
       'prev_text' => '<',
       'next_text' => '>',
-      'format' => '?paged=%#%',
   
       'total' => ceil($event_count / $per_page),
       'current' => max(1, get_query_var('paged')),
