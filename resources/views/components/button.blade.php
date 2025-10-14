95b15271297464e0933bde8bbff0e449dd0958ea
John Puddephatt <john@jdp.org.uk>
2025-10-14 19:29:09 +0100

    onclick

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
-@props(['label', 'url', 'invert' => false])
+@props(['label', 'url', 'invert' => false, 'onclick' => null])
 
-<a href="{{ $url }}"
+<a href="{{ $url }}" {{ $onclick ? ' onclick="' . $onclick . '"' : '' }}>
   {{ $attributes->merge(['class' => ($invert ? 'bg-blue-bright ' : '') . ' border-blue-bright inline-block rounded-small border-2 px-10 py-2 font-semibold !no-underline transition duration-300 hover:bg-green hover:bg-opacity-20']) }}>{{ $label }}</a>
